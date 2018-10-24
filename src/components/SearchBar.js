885c54828993232b7e4c9e81d8a4db6222c5954f
Anthony Parrett <anthonyparrett7@gmail.com>
2018-10-24 14:40:30 -0500

    Display city search results

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ import searchIcon from './assets/search-icon.png';
 import { findCities } from '../service/geo';
 
 class SearchBar extends Component {
-  state = { city: null, citiesFound: [] };
+  state = { city: null, citiesFound: [], showResults: false, doneLoading: false };
   
   handleChange(e) {
     this.setState({ city: e.target.value });
@@ -12,8 +12,21 @@ class SearchBar extends Component {
 
   async handleSubmit(e) {
     e.preventDefault();
+
+    this.setState({ doneLoading: false });
+
     const citiesFound = await findCities(this.state.city);
-    this.setState({ citiesFound });
+    this.setState({ citiesFound, showResults: true, doneLoading: true });
+  }
+
+  renderResultsList() {
+    const cities = this.state.citiesFound;
+
+    if (cities.length === 0) return <div className="no-results">No items to display.</div>;
+
+    return this.state.citiesFound.map(city => 
+      <li key={city.adminCode2}>{`${city.placeName}, ${city.adminName1}, ${city.countryCode}`}</li>
+    );
   }
 
   render() {
@@ -29,6 +42,16 @@ class SearchBar extends Component {
               <form onSubmit={this.handleSubmit.bind(this)}>
                 <input type="text" placeholder="Show me the weather in..." onChange={this.handleChange.bind(this)} />
               </form>
+              {this.state.showResults &&
+                <div className="searchBar__results">
+                  {!this.state.doneLoading && 
+                    <div className="loadingBar"><div className="loadingBar__bar"/></div>}
+                  <div className="title">Search Results</div>
+                  <ul>
+                    {this.renderResultsList()}
+                  </ul>
+                </div>
+              }
             </div>
           </div>
         </div>
--- src/service/geo.js
+++ src/service/geo.js
@@ -1,18 +1,16 @@
 import axios from 'axios';
 
+const hasUniqueZipCode = (cities, city, i) => 
+  cities.findIndex((cInner) => cInner.adminCode2 === city.adminCode2) === i;
+
+const hasUniqueNameAndState = (cities, city, i) =>
+  cities.findIndex((cInner) => cInner.placeName === city.placeName && cInner.adminName1 === city.adminName1) === i;
+
 export const findCities = async city => {
   const url = 'http://api.geonames.org/postalCodeSearchJSON?username=anthonyparrett7';
   const res = await axios.get(`${url}&placename=${city}`);
   let cities = res.data.postalCodes;
-  
-  // Filter duplicates. Result set returns duplicates for each zip code of a city.
-  cities = cities.filter((c, i) => cities.findIndex((cInner) => cInner.adminCode2 === c.adminCode2) === i);
-
-  // If search term is more than one word, theres no need to exclude the county results.
-  if (city.split(' ').length === 1 && city.search(',') === -1) {
-    // Exclude the county results.
-    cities = cities.filter(c => c.placeName.toLowerCase().search(city.toLowerCase()) !== -1 || c.postalCode.search(city) !== -1);
-  }
-
+  cities = cities.filter((c, i) => hasUniqueZipCode(cities, c, i) && hasUniqueNameAndState(cities, c, i));
+  cities = cities.sort((a, b) => a.placeName.localeCompare(b.placeName));
   return cities;
-}
+};
