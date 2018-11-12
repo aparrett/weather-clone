15b93912b0d7f315e2917c2f5c6193aa6d5d8c92
Anthony Parrett <anthonyparrett7@gmail.com>
2018-11-12 10:42:42 -0600

    Give CityBar cities unique keys

--- src/components/header/CityBar.js
+++ src/components/header/CityBar.js
@@ -3,7 +3,11 @@ import './CityBar.scss';
 
 const CityBar = ({ cities }) => (
   <div className="cityBar">
-    {cities.slice(0, 4).map(city => <div key={city.adminCode2} className="cityBar__city">{city.shortName}</div>)}
+    {cities.slice(0, 4).map(city => 
+      <div key={`${Math.abs(city.lat)}${Math.abs(city.lng)}`} className="cityBar__city">
+        {city.shortName}
+      </div>
+    )}
   </div>
 );
 
