7e018a0c1c3a73c4d856cb9afb05a4260cb24200
aparrett <anthonyparrett7@gmail.com>
2018-11-21 10:43:36 -0600

    Create Today route

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../pages/home/Home';
+import Today from '../pages/today/Today';
 
 const Router = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/today" component={Today} />
   </Switch>
 );
 
---
+++ src/pages/today/Today.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Today = () => (
+  <div className="main-container">
+    Today
+  </div>
+);
+
+export default Today;
